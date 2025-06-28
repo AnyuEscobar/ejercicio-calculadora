12345eef8ceaa1b95cba79c1b14c748a5faf892e
AnyuEscobar <mdlaescobarr@gmail.com>
2025-06-28 12:35:04 -0300

    modificacion de js y detalles de styles

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ let $result = document.getElementById("result")
 $form.addEventListener("submit", function (evento) {
   evento.preventDefault()
 
+  let parseOneInput = parseInt($inputOne.value) || 0
+  let parseTwoInput = parseInt($inputTwo.value) || 0
+
+  $result.textContent = parseOneInput + parseTwoInput
+
+
+  $inputOne.value = "";
+  $inputTwo.value = "";
 })
+
