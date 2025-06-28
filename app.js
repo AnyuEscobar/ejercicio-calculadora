b95694a10834825cd1bb919ad6438ab6d4583755
AnyuEscobar <mdlaescobarr@gmail.com>
2025-06-28 10:36:23 -0300

    modificacion de styles y primeras variables declaradas

---
+++ app.js
@@ -0,0 +1,9 @@
+let $form = document.getElementById("form")
+let $inputOne = document.getElementById("inputOne")
+let $inputTwo = document.getElementById("inputTwo")
+let $result = document.getElementById("result")
+
+$form.addEventListener("submit", function (evento) {
+  evento.preventDefault()
+
+})
